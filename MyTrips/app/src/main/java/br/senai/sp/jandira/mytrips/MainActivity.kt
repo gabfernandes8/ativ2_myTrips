5f76c159ddc1483656d74b59896ef019dad93c7f
Gabriela Fernandes <gabriela.cavalcanti886@gmail.com>
2024-03-14 15:49:02 -0300

    finalizado

--- MyTrips/app/src/main/java/br/senai/sp/jandira/mytrips/MainActivity.kt
+++ MyTrips/app/src/main/java/br/senai/sp/jandira/mytrips/MainActivity.kt
@@ -7,8 +7,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
@@ -20,7 +22,9 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddAPhoto
 import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.CameraAlt
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
@@ -30,6 +34,7 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -40,6 +45,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -233,7 +239,7 @@ fun SignUp() {
     Column(
         modifier = Modifier
             .fillMaxSize(),
-        verticalArrangement = Arrangement.SpaceAround
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
 
         Row(
@@ -254,12 +260,13 @@ fun SignUp() {
 
         Column(
             modifier = Modifier
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .height(200.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Text(
                 text = "Sign Up",
-                color = Color(0xffcf06f0),
+                color = Color(0xffcf05f0),
                 fontSize = 35.sp,
                 fontWeight = FontWeight.ExtraBold,
                 modifier = Modifier
@@ -269,17 +276,67 @@ fun SignUp() {
                 text = "Create a new account",
                 color = Color(0xffa09c9c)
             )
-
-            Card {
-
-            }
+            Box(modifier = Modifier
+                .fillMaxHeight()
+                .width(130.dp)
+                .padding(20.dp)
+            ) {
+                    Card(modifier = Modifier
+                        .fillMaxHeight()
+                        .fillMaxWidth(),
+                        border = BorderStroke(2.dp, Brush.horizontalGradient(listOf(Color(0xffcf06f0), Color(0x1acf06f0)))),
+                        colors = CardDefaults.cardColors(Color(0xffF6F6F6)),
+                    shape = RoundedCornerShape(100.dp)
+                        ) {
+                        Row (
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.Center,
+                            modifier = Modifier
+                                .fillMaxSize()
+                        ) {
+                            Icon(
+                                imageVector = Icons.Default.Person,
+                                contentDescription = "",
+                                tint = Color(0xffcf06f0),
+                                modifier = Modifier
+                                    .size(85.dp)
+                            )
+                        }
+                    }
+                Card (
+                    modifier = Modifier
+                        .align(Alignment.BottomEnd)
+                        .background(Color.Transparent)
+                        .height(20.dp),
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color.Transparent
+                    )
+                ) {
+                    Icon(
+                        imageVector = Icons.Default.AddAPhoto,
+                        contentDescription = "",
+                        tint = Color(0xffCB27A8),
+                        modifier = Modifier
+                            .size(25.dp)
+                    )
+                }
+                }
         }
 
-        Column {
+        Column(modifier = Modifier
+            .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween
+        ) {
 
             OutlinedTextField(
-                modifier = Modifier,
-                value = "",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        end = 15.dp,
+                        start = 15.dp
+                    ),
+                value = "Gabriela Fernandes",
                 onValueChange = {},
                 leadingIcon = {
                     Icon(
@@ -305,8 +362,14 @@ fun SignUp() {
             )
 
             OutlinedTextField(
-                modifier = Modifier,
-                value = "",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        top = 15.dp,
+                        end = 15.dp,
+                        start = 15.dp
+                    ),
+                value = "(11) 91234-1234",
                 onValueChange = {},
                 leadingIcon = {
                     Icon(
@@ -331,8 +394,14 @@ fun SignUp() {
             )
 
             OutlinedTextField(
-                modifier = Modifier,
-                value = "",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        top = 15.dp,
+                        end = 15.dp,
+                        start = 15.dp
+                    ),
+                value = "gab@gmail.com",
                 onValueChange = {},
                 leadingIcon = {
                     Icon(
@@ -357,8 +426,14 @@ fun SignUp() {
             )
 
             OutlinedTextField(
-                modifier = Modifier,
-                value = "",
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        top = 15.dp,
+                        end = 15.dp,
+                        start = 15.dp
+                    ),
+                value = "al13",
                 onValueChange = {},
                 leadingIcon = {
                     Icon(
@@ -385,13 +460,18 @@ fun SignUp() {
 
         Row(
             modifier = Modifier
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .padding(top = 5.dp),
             verticalAlignment = Alignment.CenterVertically
         ) {
             Checkbox(
                 checked = false, onCheckedChange = {},
                 modifier = Modifier
-                    .height(50.dp)
+                    .height(30.dp),
+                colors = CheckboxDefaults.colors(
+                    checkedColor = Color(0xffcf06f0),
+                    uncheckedColor = Color(0xffcf06f0)
+                )
             )
             Text(
                 text = "Over 18?"
@@ -400,11 +480,13 @@ fun SignUp() {
 
         Button(modifier = Modifier
             .fillMaxWidth()
-            .padding(15.dp),
+            .padding(
+                end = 15.dp,
+                start = 15.dp),
             onClick = { /*TODO*/ },
             shape = RoundedCornerShape(12.dp),
             colors = ButtonDefaults.buttonColors(
-                containerColor = Color(0xffcf06f0))
+                containerColor = Color(0xffcf05f0))
         ) {
             Text(
                 text = "CREATE ACCOUNT",
@@ -424,7 +506,8 @@ fun SignUp() {
                 text = "Already have an account?",
                 color = Color(0xffa09c9c)
             )
-            Text(
+            Text(modifier = Modifier
+                .padding(end = 15.dp),
                 text = "Sign in",
                 color = Color(0xffcf06f0),
                 fontWeight = FontWeight.ExtraBold
@@ -454,9 +537,9 @@ fun SignUp() {
 //@Preview(showBackground = true, showSystemUi = true)
 //@Composable
 //fun LoginPreview() {
-  //  MyTripsTheme {
-    //    Login()
-    //}
+//    MyTripsTheme {
+//        Login()
+//    }
 //}
 
 @Preview(showBackground = true, showSystemUi = true)
